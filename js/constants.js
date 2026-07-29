4b4deae07a696b7a66f2cbf821fb4fc75acb4404
Cyberdeall <fadilputunembahe@gmail.com>
2026-07-30 01:38:22 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -64,7 +64,7 @@ const CONSTANTS = Object.freeze({
 
     // ========== CLERK AUTH ==========
     CLERK: {
-        PUBLISHABLE_KEY: "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_ZGFzaGluZy1idWctNjMuY2xlcmsuYWNjb3VudHMuZGV2JA==",
+        PUBLISHABLE_KEY: "pk_test_ZGFzaGluZy1idWctNjMuY2xlcmsuYWNjb3VudHMuZGV2JA==",
         TIMEOUT_MS: 10000,              // timeout untuk Clerk API
     },
 });
